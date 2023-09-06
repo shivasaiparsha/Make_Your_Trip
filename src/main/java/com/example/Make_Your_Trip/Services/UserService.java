4ecab791a5700be20986a74a4893e2693a5dd4c2
unknown <shivasaiparsha140@gmail.com>
2023-09-06 17:37:53 +0530

    swagger+Email

--- src/main/java/com/example/Make_Your_Trip/Services/UserService.java
+++ src/main/java/com/example/Make_Your_Trip/Services/UserService.java
@@ -4,6 +4,8 @@ import com.example.Make_Your_Trip.Models.User;
 import com.example.Make_Your_Trip.Repositories.UserRepository;
 import com.example.Make_Your_Trip.RequestDto.AddUserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +14,24 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JavaMailSender mailSender;
     public String addUser(AddUserDto addUserDto) {
 
         User user =User.builder().age(addUserDto.getAge())
                 .name(addUserDto.getName())
                 .emailId(addUserDto.getEmailId())
                 .build();
-        userRepository.save(user);
 
-  return "User Successfully added";
+        SimpleMailMessage mailMessage=new SimpleMailMessage();
+        String body="Hi Welcome to MakeYourTrip \n"+"Make Youre Tours plans Here";
+        mailMessage.setSubject("Make Your Trip");
+        mailMessage.setFrom("xyz@gmail.com");
+        mailMessage.setTo(user.getEmailId());
+        mailMessage.setText(body);
+        mailSender.send(mailMessage);
+
+       return "User Successfully added";
+
     }
 }
